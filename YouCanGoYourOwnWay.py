105234eec6dbf36cdf40cafc7d6f3de6f95dbc55
AYUSH RAJ <31946526+breaker9@users.noreply.github.com>
2019-07-12 19:37:44 +0530

    Add files via upload

---
+++ YouCanGoYourOwnWay.py
@@ -0,0 +1,23 @@
+test = int(input())
+for i in range(test):
+    dim = int(input())
+    path = input()
+    path= path.replace("S","L").replace("E","S").replace("L","E")
+    print("Case #"+str(i+1)+":",path)
+        
+
+
+
+
+
+
+
+
+
+    
+        
+
+
+        
+        
+
